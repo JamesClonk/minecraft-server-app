1c60ab9acf049aedca707521e9d193f4c470add2
JamesClonk <jamesclonk@jamesclonk.ch>
2018-04-27 22:16:17 +0200

    add rcon

--- main.go
+++ main.go
@@ -2,28 +2,52 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/JamesClonk/minecraft-server-app/env"
 	cfenv "github.com/cloudfoundry-community/go-cfenv"
+	"github.com/james4k/rcon"
 	minio "github.com/minio/minio-go"
 )
 
-func main() {
-	env, err := cfenv.Current()
+var (
+	s3Client   *minio.Client
+	bucketName = "world-backup"
+)
+
+func init() {
+	app, err := cfenv.Current()
 	if err != nil {
 		log.Fatalf("could not parse VCAP environment: %s\n", err)
 	}
 
-	service, err := env.Services.WithName("world-backup")
+	service, err := app.Services.WithName("world-backup")
 	if err != nil {
 		log.Fatalf("could not get world-backup service from VCAP environment: %s\n", err)
 	}
 
+	bucketName = env.MustGet("MINECRAFT_BACKUP_BUCKET_NAME")
+	endpoint, _ := service.CredentialString("accessHost")
+	accessKeyID, _ := service.CredentialString("accessKey")
+	secretAccessKey, _ := service.CredentialString("sharedSecret")
+	useSSL := true
+
+	s3Client, err = minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+	if err != nil {
+		log.Fatalf("could not initialize minio s3 client: %s\n", err)
+	}
+}
+
+func main() {
+	restoreBackup()
+	go createBackups()
 	startServer()
-	downloadBackup(service)
 }
 
 func startServer() {
@@ -33,6 +57,9 @@ func startServer() {
 	}
 	path := env.MustGet("PATH")
 	os.Setenv("PATH", fmt.Sprintf("%s:%s/.java-buildpack/open_jdk_jre/bin", path, pwd))
+
+	// update server.properties
+	modifyServerProperties("rcon.password", env.MustGet("MINECRAFT_RCON_PASSWORD"))
 
 	cmd := exec.Command("java", "-Xmx1024M", "-Xms1024M", "-jar", "minecraft.jar", "nogui")
 	cmd.Stdout = os.Stdout
@@ -46,19 +73,93 @@ func startServer() {
 	}
 }
 
-func downloadBackup(service *cfenv.Service) {
-	bucketName := env.MustGet("BUCKET_NAME")
-	endpoint, _ := service.CredentialString("accessHost")
-	accessKeyID, _ := service.CredentialString("accessKey")
-	secretAccessKey, _ := service.CredentialString("sharedSecret")
-	useSSL := true
-
-	minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+func modifyServerProperties(property, value string) {
+	input, err := ioutil.ReadFile("server.properties")
 	if err != nil {
-		log.Fatalf("could not initialize minio s3 client: %s\n", err)
+		log.Fatalf("could not read server.properties: %s\n", err)
 	}
 
-	if err := minioClient.FGetObject(bucketName, "world-backup.zip", "world-backup.zip", minio.GetObjectOptions{}); err != nil {
-		log.Fatalf("could not download world-backup.zip: %s\n", err)
+	lines := strings.Split(string(input), "\n")
+	for i, line := range lines {
+		if strings.Contains(line, property) {
+			lines[i] = property + "=" + value
+		}
+	}
+
+	if err := ioutil.WriteFile("server.properties", []byte(strings.Join(lines, "\n")), 0644); err != nil {
+		log.Fatalf("could not write server.properties: %s\n", err)
+	}
+}
+
+func createBackups() {
+	// backup world every 15 minutes, starting immediately an initial after 3min delay
+	time.Sleep(3 * time.Minute)
+	for {
+		rconExec("say Starting backup now...")
+		rconExec("save-off")
+		rconExec("save-all")
+		time.Sleep(5 * time.Second)
+
+		os.Remove("world-backup.tar.gz")
+		cmd := exec.Command("tar", "-cvzf", "world-backup.tar.gz", "*")
+		cmd.Dir = "world"
+		if err := cmd.Run(); err != nil {
+			log.Fatalf("could not backup world: %s\n", err)
+		}
+		rconExec("save-on")
+
+		if _, err := s3Client.FPutObject(bucketName, "world-backup.tar.gz", "world-backup.tar.gz",
+			minio.PutObjectOptions{ContentType: "application/gzip"}); err != nil {
+			log.Fatalf("could not upload world backup: %s\n", err)
+		}
+		rconExec("say Backup complete!")
+
+		time.Sleep(15 * time.Minute)
+	}
+}
+
+func restoreBackup() {
+	if err := s3Client.MakeBucket(bucketName, ""); err != nil {
+		if exists, err := s3Client.BucketExists(bucketName); err != nil || !exists {
+			log.Fatalf("could not create bucket [%s]: %s\n", bucketName, err)
+		}
+	}
+
+	info, err := s3Client.StatObject(bucketName, "world-backup.tar.gz", minio.StatObjectOptions{})
+	if err == nil && info.Size > 0 {
+		os.Remove("world-backup.tar.gz")
+		if err := s3Client.FGetObject(bucketName, "world-backup.tar.gz", "world-backup.tar.gz", minio.GetObjectOptions{}); err != nil {
+			log.Fatalf("could not download world backup: %s\n", err)
+		}
+
+		if err := os.RemoveAll("world/"); err != nil {
+			log.Fatalf("could not delete world folder: %s\n", err)
+		}
+
+		cmd := exec.Command("tar", "-xvzf", "world-backup.tar.gz", "-C", "world")
+		if err := cmd.Run(); err != nil {
+			log.Fatalf("could not restore backup world: %s\n", err)
+		}
+	}
+}
+
+func rconExec(command string) {
+	console, err := rcon.Dial("localhost", env.MustGet("MINECRAFT_RCON_PASSWORD"))
+	if err != nil {
+		log.Fatalf("failed to connect to minecraft rcon server: %s\n", err)
+	}
+	defer console.Close()
+
+	reqId, err := console.Write(command)
+	_, respReqId, err := console.Read()
+	if err != nil {
+		if err == io.EOF {
+			return
+		}
+		log.Fatalf("failed to read rcon command: %s\n", err)
+	}
+
+	if reqId != respReqId {
+		fmt.Fprintln(os.Stderr, "response was for another request!")
 	}
 }
---
+++ vendor/github.com/james4k/rcon/rcon_test.go
@@ -0,0 +1,181 @@
+package rcon
+
+import (
+	"bytes"
+	"encoding/binary"
+	"net"
+	"testing"
+)
+
+func startTestServer(fn func(net.Conn, *bytes.Buffer)) (string, error) {
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return "", err
+	}
+
+	go func() {
+		conn, err := listener.Accept()
+		if err != nil {
+			return
+		}
+		defer conn.Close()
+
+		buf := make([]byte, readBufferSize)
+		_, err = conn.Read(buf)
+		if err != nil {
+			return
+		}
+
+		var packetSize, requestId, cmdType int32
+		var str []byte
+		b := bytes.NewBuffer(buf)
+		binary.Read(b, binary.LittleEndian, &packetSize)
+		binary.Read(b, binary.LittleEndian, &requestId)
+		binary.Read(b, binary.LittleEndian, &cmdType)
+		str, err = b.ReadBytes(0x00)
+		if err != nil {
+			return
+		}
+		if string(str[:len(str)-1]) != "blerg" {
+			requestId = -1
+		}
+
+		b.Reset()
+		binary.Write(b, binary.LittleEndian, int32(10))
+		binary.Write(b, binary.LittleEndian, int32(requestId))
+		binary.Write(b, binary.LittleEndian, int32(respAuthResponse))
+		binary.Write(b, binary.LittleEndian, byte(0))
+		binary.Write(b, binary.LittleEndian, byte(0))
+		conn.Write(b.Bytes())
+
+		if fn != nil {
+			b.Reset()
+			fn(conn, b)
+		}
+	}()
+
+	return listener.Addr().String(), nil
+}
+
+func TestAuth(t *testing.T) {
+	addr, err := startTestServer(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rc, err := Dial(addr, "blerg")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = rc.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestMultipacket(t *testing.T) {
+	addr, err := startTestServer(func(c net.Conn, b *bytes.Buffer) {
+		// start packet
+		// start response
+		binary.Write(b, binary.LittleEndian, int32(10+4000))
+		binary.Write(b, binary.LittleEndian, int32(123))
+		binary.Write(b, binary.LittleEndian, int32(respResponse))
+		for i := 0; i < 4000; i += 1 {
+			binary.Write(b, binary.LittleEndian, byte(' '))
+		}
+		binary.Write(b, binary.LittleEndian, byte(0))
+		binary.Write(b, binary.LittleEndian, byte(0))
+		// end response
+		// start response
+		binary.Write(b, binary.LittleEndian, int32(10+4000))
+		binary.Write(b, binary.LittleEndian, int32(123))
+		binary.Write(b, binary.LittleEndian, int32(respResponse))
+		for i := 0; i < 2000; i += 1 {
+			binary.Write(b, binary.LittleEndian, byte(' '))
+		}
+		c.Write(b.Bytes())
+		// end packet
+
+		// start packet
+		b.Reset()
+		for i := 0; i < 2000; i += 1 {
+			binary.Write(b, binary.LittleEndian, byte(' '))
+		}
+		binary.Write(b, binary.LittleEndian, byte(0))
+		binary.Write(b, binary.LittleEndian, byte(0))
+		// end response
+		// start response
+		binary.Write(b, binary.LittleEndian, int32(10+2000))
+		binary.Write(b, binary.LittleEndian, int32(123))
+		binary.Write(b, binary.LittleEndian, int32(respResponse))
+		for i := 0; i < 2000; i += 1 {
+			binary.Write(b, binary.LittleEndian, byte(' '))
+		}
+		binary.Write(b, binary.LittleEndian, byte(0))
+		binary.Write(b, binary.LittleEndian, byte(0))
+		// end response
+		// start response - size word is split!
+		binary.Write(b, binary.LittleEndian, int32(10+2000))
+		c.Write(b.Bytes()[:len(b.Bytes())-3])
+		// end packet
+
+		b.Reset()
+		binary.Write(b, binary.LittleEndian, int32(10+2000))
+		binary.Write(b, binary.LittleEndian, int32(123))
+		binary.Write(b, binary.LittleEndian, int32(respResponse))
+		for i := 0; i < 2000; i += 1 {
+			binary.Write(b, binary.LittleEndian, byte(' '))
+		}
+		binary.Write(b, binary.LittleEndian, byte(0))
+		binary.Write(b, binary.LittleEndian, byte(0))
+		// end response
+		c.Write(b.Bytes()[1:])
+		// end packet
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rc, err := Dial(addr, "blerg")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	str, _, err := rc.Read()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(str) != 4000 {
+		t.Fatal("response length not correct")
+	}
+
+	str, _, err = rc.Read()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(str) != 4000 {
+		t.Fatal("response length not correct")
+	}
+
+	str, _, err = rc.Read()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(str) != 2000 {
+		t.Fatal("response length not correct")
+	}
+
+	str, _, err = rc.Read()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(str) != 2000 {
+		t.Fatal("response length not correct")
+	}
+
+	err = rc.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
