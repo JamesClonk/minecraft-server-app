13a7852022c89b54bb59f6fdd2cd09b2ed544dde
JamesClonk <jamesclonk@jamesclonk.ch>
2018-04-27 14:24:32 +0200

    add minio client

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/cloudfoundry-community/go-cfenv"
+	"github.com/JamesClonk/minecraft-server-app/env"
+	cfenv "github.com/cloudfoundry-community/go-cfenv"
+	minio "github.com/minio/minio-go"
 )
 
 func main() {
@@ -17,5 +18,22 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not get world-backup service from VCAP environment: %s\n", err)
 	}
-	fmt.Println(service)
+	downloadBackup(service)
+}
+
+func downloadBackup(service *cfenv.Service) {
+	bucketName := env.MustGet("BUCKET_NAME")
+	endpoint, _ := service.CredentialString("accessHost")
+	accessKeyID, _ := service.CredentialString("accessKey")
+	secretAccessKey, _ := service.CredentialString("sharedSecret")
+	useSSL := true
+
+	minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+	if err != nil {
+		log.Fatalf("could not initialize minio s3 client: %s\n", err)
+	}
+
+	if err := minioClient.FGetObject(bucketName, "world-backup.zip", "world-backup.zip", minio.GetObjectOptions{}); err != nil {
+		log.Fatalf("could not download world-backup.zip: %s\n", err)
+	}
 }
